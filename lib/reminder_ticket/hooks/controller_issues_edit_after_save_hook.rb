886f8ad7a066d6225506e6c2e7cb86a0b760115e
Souichi Saitou <souichi.saitou@gmail.com>
2012-02-03 17:11:28 +0900

    hoge

--- lib/reminder_ticket/hooks/controller_issues_edit_after_save_hook.rb
+++ lib/reminder_ticket/hooks/controller_issues_edit_after_save_hook.rb
@@ -2,10 +2,14 @@ module ReminderTicket
   module Hooks
     class ControllerIssuesEditAfterSaveHook < Redmine::Hook::ViewListener
       def controller_issues_edit_after_save(context={})
-        issue = context[:issue]
-        if issue.tracker_id == 4 and (issue.closing? or issue.closed?)
-          tn = issue.id
-          system "schtasks /delete /f /tn #{tn}"
+        begin
+          issue = context[:issue]
+          if issue.tracker_id == 4 and (issue.closing? or issue.closed?)
+            tn = issue.id
+            return system "schtasks /delete /f /tn #{tn}"
+          end
+        rescue
+          return false
         end
       end
     end
--- lib/reminder_ticket/hooks/controller_issues_new_after_save_hook.rb
+++ lib/reminder_ticket/hooks/controller_issues_new_after_save_hook.rb
@@ -2,19 +2,23 @@ module ReminderTicket
   module Hooks
     class ControllerIssuesNewAfterSaveHooks < Redmine::Hook::ViewListener
       def controller_issues_new_after_save(context={})
-        issue = context[:issue]
-        if issue.tracker_id == 4
-          tn = issue.id
-          tr = "#{RAILS_ROOT}\\vendor\\plugins\\redmine_reminder_ticket\\lib\\bat\\reminder_ticket.bat #{issue.id}"
-          sc = "once"
-          sd = issue.start_date.strftime("%Y/%m/%d")
-          st = (Time.parse("#{sd} #{issue.custom_field_values[0]}") - 3600).strftime("%H:%M")
-          system "schtasks /create /tn #{tn} /tr \"#{tr}\" /sc #{sc} /sd #{sd} /st #{st}"
+        begin
+          issue = context[:issue]
+          if issue.tracker_id == 4
+            tn = issue.id
+            tr = "#{RAILS_ROOT}/vendor/plugins/redmine_reminder_ticket/lib/bat/reminder_ticket.bat #{issue.id}"
+            sc = "once"
+            sd = issue.start_date.strftime("%Y/%m/%d")
+            st = (Time.parse("#{sd} #{issue.custom_field_values[0]}") - 3600).strftime("%H:%M")
+            return system "schtasks /create /tn #{tn} /tr \"#{tr}\" /sc #{sc} /sd #{sd} /st #{st}"
 
-          # required user & password
-          # ru = ""
-          # rp = ""
-          # system "schtasks /create /tn #{tn} /tr \"#{tr}\" /sc #{sc} /sd #{sd} /st #{st} /ru #{ru} /rp #{rp}"
+            # required user & password
+            # ru = ""
+            # rp = ""
+            # return system "schtasks /create /tn #{tn} /tr \"#{tr}\" /sc #{sc} /sd #{sd} /st #{st} /ru #{ru} /rp #{rp}"
+          end
+        rescue
+          return false
         end
       end
     end
