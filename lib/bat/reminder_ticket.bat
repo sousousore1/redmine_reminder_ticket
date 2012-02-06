b1707dc19f388c3f9a0a6fe50fd64ff0ad04b9df
Souichi Saitou <souichi.saitou@gmail.com>
2012-02-06 12:05:17 +0900

    hoge

---
+++ lib/bat/reminder_ticket.bat
@@ -0,0 +1,2 @@
+cd C:\redmine
+rake redmine:send_reminder_ticket issue_id=%1 RAILS_ENV=production
--- lib/bat/reminder_ticket_task.rb
+++
@@ -1,2 +0,0 @@
-system "cd #{RAILS_ROOT}"
-system "rake redmine:send_reminder_ticket issue_id=#{ARGV[0]} RAILS_ENV=#{RAILS_ENV}"
